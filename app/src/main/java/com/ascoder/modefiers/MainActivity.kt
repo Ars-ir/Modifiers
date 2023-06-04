ef93e390e37bb181004444ac1684f784a0cd2be9
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-04 20:53:23 +0700

    - Create Project Modifiers

---
+++ app/src/main/java/com/ascoder/modefiers/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ascoder.modefiers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ascoder.modefiers.ui.theme.ModefiersTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .width(300.dp)
+                    .requiredWidth(300.dp)
+            ) {
+                Text(text = "Hello")
+                Text(text = "World!")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ModefiersTheme {
+        Greeting("Android")
+    }
+}
